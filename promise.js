3b41af511285b2c79767dbbfc2dd1f903ef62df1
ATharushika <atharushika50@gmail.com>
2026-02-16 13:08:22 +0530

    add promise.js

---
+++ promise.js
@@ -0,0 +1,14 @@
+const condition = true; // or false
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve('Success!');
+  } else {
+    reject('Failure!');
+  }
+});
+
+myPromise.then((result) => {
+  console.log(result);
+}).catch((error) => {
+  console.log(error);
+});
